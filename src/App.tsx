99c21e4d88c58d7c52e91db97fb42c3eebe731cc
anuj-kumary <anujf0510@gmail.com>
2022-07-15 15:52:40 +0530

    feat:Devcha Generator

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Devcha } from './components/Devcha';
+
+function App() {
+  console.log("Call getDevcha function to get you devcha code")
+  return (
+    <div className='App'>
+    <Devcha />
+    </div>
+  );
+}
+
+export default App;
